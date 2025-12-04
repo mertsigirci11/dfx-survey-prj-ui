654c84f0835a6ef93f00c4c251ca38fd614bf05f
BurakKontas <aburakkontas@hotmail.com>
2025-12-04 11:59:02 +0300

    initialize

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import { PrimeReactProvider } from 'primereact/api';
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import './index.css'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <PrimeReactProvider>
+    <App />
+  </PrimeReactProvider>,
+)

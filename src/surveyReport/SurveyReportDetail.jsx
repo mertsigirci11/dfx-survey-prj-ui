fdaa56b6901dac4c8ab28632aeed7b774c606831
Mert Sigirci <mertsgrci@hotmail.com>
2025-12-04 16:51:26 +0300

    rapor

---
+++ src/surveyReport/SurveyReportDetail.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Card } from "primereact/card";
+import axios from "axios";
+import { redirect } from "react-router-dom";
+import "../surveyDetail.css";
+
+export default function SurveyReport(){
+    return(
+        <div>
+           <div class="avg"></div> 
+        </div>
+    );
+}

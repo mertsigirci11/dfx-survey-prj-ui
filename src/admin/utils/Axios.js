6f1f3f9d1c3db8f7099705ddc1ec3a7ce6126fb1
BurakKontas <aburakkontas@hotmail.com>
2025-12-04 14:24:02 +0300

    survey list and duplicate

---
+++ src/admin/utils/Axios.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: "http://localhost:8081",
+    withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            const parsed = JSON.parse(token);
+            config.headers.Authorization = `Bearer ${parsed.token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// RESPONSE INTERCEPTOR
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        // örnek: 401 olursa refresh deneyelim
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const refreshToken = JSON.parse(localStorage.getItem("token"))?.refreshToken;
+
+                const res = await axios.post(
+                    "http://localhost:8081/refresh?token=" + refreshToken
+                );
+
+                if (res.data.success) {
+                    const newToken = {
+                        token: res.data.data.token,
+                        refreshToken: res.data.data.refreshToken,
+                    };
+
+                    localStorage.setItem("token", JSON.stringify(newToken));
+
+                    originalRequest.headers.Authorization = `Bearer ${newToken.token}`;
+
+                    return axiosInstance(originalRequest);
+                }
+            } catch (refreshErr) {
+                localStorage.removeItem("token");
+                window.location.href = "/admin/logout";
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,13 @@ import {
   createBrowserRouter
 } from "react-router-dom";
 
+import AdminLayout from "./admin/layouts/AdminLayout";
+import Surveys from "./admin/pages/Surveys";
+import SurveyDetails from "./admin/pages/SurveyDetails";
+
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "./index.css";
+import Logout from "./admin/pages/Logout";
 
 const router = createBrowserRouter([
   {
@@ -14,8 +19,21 @@ const router = createBrowserRouter([
     element: <div>PlaceHolder</div>,
     children: [
       { index: true, element: <div>PlaceHolder</div>, },
-      { path: "about", element: <div>PlaceHolder</div>, },
-      { path: "*", element: <div>PlaceHolder</div>, }
+    ]
+  },
+  {
+    path: "/admin",
+    children: [
+      { path: "login", element: <div>Login</div>, },
+      { path: "logout", element: <Logout />, }
+    ]
+  },
+  {
+    path: "/admin/survey",
+    element: <AdminLayout />,
+    children: [
+      { index: true, element: <Surveys />, },
+      { path: ":id", element: <SurveyDetails />, }
     ]
   }
 ]);
